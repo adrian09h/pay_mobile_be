1405f6d3359169faaf6403498ed851b1fe0c4e33
Adrian Rusin <adrian09h@gmail.com>
2023-08-28 09:56:41 +0200

    Update forgotPassword to return body

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -28,7 +28,7 @@ const refreshTokens = catchAsync(async (req, res) => {
 const forgotPassword = catchAsync(async (req, res) => {
   const resetPasswordToken = await tokenService.generateResetPasswordToken(req.body.email);
   await emailService.sendResetPasswordEmail(req.body.email, resetPasswordToken);
-  res.status(httpStatus.NO_CONTENT).send();
+  res.send({ success: true }).send();
 });
 
 const resetPassword = catchAsync(async (req, res) => {
